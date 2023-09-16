e5b62ef3b21c052e0f8e1c870ba15ef39b315e76
Dinesh Kuddana <117835803+DineshKuddana@users.noreply.github.com>
2023-09-17 01:04:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,116 @@
+
+function App() {
+  return (
+    <div className="App">
+        <nav className="navbar">
+          <div className="left">
+            <h1>ASB Design</h1>
+          </div>
+          <div className="middle">
+            <ul>
+              <li>Services</li>
+              <li>Portfolio</li>
+              <li>Pricing</li>
+            </ul>
+          </div>
+          <div className="right">
+            <button>Contact Us</button>
+          </div>
+        </nav>
+
+        <div className="content">
+          <div className="left-content">
+            <h2 className="one">Unleash Creativity</h2>
+            <h2 className="two">Find Employment</h2>
+            <h2 className="three">Bolster Economy</h2>
+            <p>ASB Design offers not just creative education, but bridges the chasm between<br></br>
+              skills and employment opportunities,providing an effective, accessible<br></br>
+              pathway to success.
+            </p>
+            <button>
+              Get Started
+            </button>
+            </div>
+            <div className="right-content">
+            <img
+            src="https://fkcms.vkpublications.com/web/thumbnail/lg/1674626527070-0.png"
+            alt="Placeholder"
+          />
+            </div>
+        </div>
+
+
+        <div className="exclusive-courses">
+        <div className="centerr">
+          <h2>Our Exclusive Courses</h2>
+          <p>
+          With our platter of diverse, comprehensive courses,you have the key to unlock the doors of opportunity
+          and personal growth.
+          </p>
+        </div>
+        <div className="leftt">
+        <img
+            src="https://www.qualitylogoproducts.com/blog/wp-content/uploads/2019/04/why-write-things-by-hand.jpg"
+            alt="Placeholder"
+          />
+        </div>
+        <div className="rightt">
+          <div className="icon-group">
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 1"
+              />
+              <h1>Photography</h1>
+              <p>Course 2</p>
+            </div>
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 2"
+              />
+              <h1>Graphic Design</h1>
+              <p>Course 2</p>
+            </div>
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 3"
+              />
+              <h1>Graphic Design</h1>
+              <p>Course 3</p>
+            </div>
+          </div>
+          <div className="icon-group">
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 4"
+              />
+              <h1>Graphic Design</h1>
+              <p>Course 4</p>
+            </div>
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 5"
+              />
+              <h1>Graphic Design</h1>
+              <p>Course 5</p>
+            </div>
+            <div className="icon">
+              <img
+                src="https://media.istockphoto.com/id/1175387759/vector/camera-icon.jpg?s=612x612&w=0&k=20&c=paC1ZkU31dH2B5epXqT_cYOyca5uqh4v0WpFUldCUBE="
+                alt="Icon 6"
+              />
+              <h1>Graphic Design</h1>
+              <p>Course 6</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
